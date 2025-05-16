cce3f3cb388b7e4ae8305833d719b9fcf128a5af
glovric <grgo.lovric@gmail.com>
2025-05-16 11:08:08 +0200

    Added model and scaler serialization, updated streamlit

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import pandas as pd
 import yfinance as yf
 import plotly.express as px
 from streamlit_utils import *
-from utils import add_financial_features
+from utils import add_financial_features, get_financial_data
+from torch_utils import load_lstm_model, load_showcase_data
+import joblib
 
 @st.cache_data
 def load_data():
@@ -57,12 +59,11 @@ elif page == "🤖 LSTM Model Showcase":
     st.title("🤖 LSTM Model Showcase")
     st.markdown("""
         This tab is reserved for displaying predictions, evaluation metrics, and charts from your LSTM time series model.
-                
-        🧠 You can integrate:
-        - Prediction vs Actual plots
-        - RMSE, MAE, R² metrics
-        - Interactive input (e.g., forecast next N days)
     """)
+
+    df = get_financial_data()
+    lstm_model = load_lstm_model()
+    X_train_seq, X_test_seq, y_train_seq, y_test_seq = load_showcase_data(df)
 
     # Placeholder sample chart (remove or replace with your model output)
     x = np.arange(0, 100)
@@ -73,3 +74,45 @@ elif page == "🤖 LSTM Model Showcase":
     fig_lstm = px.line(df_pred, x="Time", y=["Actual", "Predicted"], title="Sample LSTM Output (Mock Data)",
                        labels={"value": "Price", "Time": "Time Step"}, template="plotly_white")
     st.plotly_chart(fig_lstm, use_container_width=True)
+    #################################################
+
+    scaler = joblib.load('scalers/standard_scaler.save')
+    y_true = scaler.inverse_transform(y_train_seq.cpu().detach().numpy())
+    y_pred = lstm_model(X_train_seq).cpu().detach().numpy()
+    y_pred = scaler.inverse_transform(y_pred)
+    line_color_true = '#1f77b4'
+    line_color_pred = '#ff7f0e'
+    line_cols = st.columns(5)
+
+    for i, feature in enumerate(df.columns):
+        col = line_cols[i % 5]
+        
+        with col:
+            idx = df.columns.get_loc(feature)  # get index of the feature in df
+
+            fig = go.Figure()
+
+            fig.add_trace(go.Scatter(
+                x=df.index, y=y_true[:, idx],
+                mode='lines', name='True', line=dict(color=line_color_true)
+            ))
+
+            fig.add_trace(go.Scatter(
+                x=df.index, y=y_pred[:, idx],
+                mode='lines', name='Predicted', line=dict(color=line_color_pred)
+            ))
+
+            fig.update_layout(
+                title=f"Line Chart of {feature}",
+                template="plotly_white",
+                legend=dict(x=0.01, y=0.99),
+                margin=dict(t=40, b=20)
+            )
+
+            fig.update_xaxes(tickformat="%Y-%m")
+
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Regenerate new columns every 2 plots
+        if (i + 1) % 5 == 0:
+            line_cols = st.columns(5)
--- torch_utils.py
+++ torch_utils.py
@@ -1,4 +1,10 @@
 from torch.utils.data import Dataset
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import torch
+import joblib
+from utils import create_sequence
 
 class StockDataset(Dataset):
 
@@ -25,3 +31,73 @@ class StockDataset(Dataset):
         sample = self.data[idx]
         target = self.targets[idx]
         return sample, target
+    
+def load_showcase_data(df, start_date: str = "2015-01-01"):
+    df = df[df.index >= start_date] # Select rows starting at start_date
+    X = df.to_numpy()
+    X_train, X_test = train_test_split(X, test_size=0.2, shuffle=False)
+    scaler = joblib.load('scalers/standard_scaler.save')
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    X_train_seq, y_train_seq = create_sequence(X_train_scaled, input_seq_len=10)
+    X_test_seq, y_test_seq = create_sequence(X_test_scaled, input_seq_len=7)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    X_train_seq, X_test_seq, y_train_seq, y_test_seq = torch.Tensor(X_train_seq), torch.Tensor(X_test_seq), torch.Tensor(y_train_seq), torch.Tensor(y_test_seq)
+    X_train_seq, X_test_seq, y_train_seq, y_test_seq = X_train_seq.to(device), X_test_seq.to(device), y_train_seq.to(device), y_test_seq.to(device)
+    return X_train_seq, X_test_seq, y_train_seq, y_test_seq
+
+def load_lstm_model(predict_sequence=False):
+    n_features = 10
+    hidden_dim = 128
+    num_layers = 3
+
+    if not predict_sequence:
+        dropout = 0.3
+        lstm_model = LSTMModel(input_dim=n_features, 
+                            output_dim=n_features, 
+                            hidden_dim=hidden_dim, 
+                            num_layers=num_layers, 
+                            dropout=dropout, 
+                            predict_sequence=predict_sequence)
+        lstm_model.load_state_dict(torch.load("models/lstm_m2o_16-05-2025.pth"))
+
+    else:
+        dropout = 0.1
+        lstm_model = LSTMModel(input_dim=n_features, 
+                        output_dim=n_features, 
+                        hidden_dim=hidden_dim, 
+                        num_layers=num_layers, 
+                        dropout=dropout, 
+                        predict_sequence=predict_sequence)
+        lstm_model.load_state_dict(torch.load("models/lstm_m2m_16-05-2025.pth"))
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    lstm_model = lstm_model.to(device)
+    return lstm_model
+
+    
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=6, dropout=0.1, predict_sequence = False):
+        super(LSTMModel, self).__init__()
+
+        self.predict_sequence = predict_sequence
+        
+        self.lstm = nn.LSTM(input_size=input_dim, 
+                            hidden_size=hidden_dim, 
+                            num_layers=num_layers, 
+                            dropout=dropout,
+                            batch_first=True)
+        
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        
+        output, (h_out, c_out) = self.lstm(x)
+        #torch.allclose(output[:, -1, :], h_out[-1]
+
+        if self.predict_sequence:
+            out = self.fc_out(output)
+        else:
+            out = self.fc_out(output[:, -1, :])
+        
+        return out
